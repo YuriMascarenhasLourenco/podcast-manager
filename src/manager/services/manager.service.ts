5b1edee0260b236184f1704f0a344eefd9876003
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-02-18 21:21:33 -0300

    fix: service error treatment

--- src/manager/services/manager.service.ts
+++ src/manager/services/manager.service.ts
@@ -1,13 +1,32 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { data } from '../types/data.interface';
 
 @Injectable()
 export class ManagerService {
   constructor(@Inject('Data') private readonly Data: data[]) {}
   getPodcasts(): data[] {
-    return this.Data;
+    try {
+      const info = this.Data;
+      if (info) {
+        return info;
+      }
+    } catch (err) {
+     throw new HttpException('there are no episodes in your list', HttpStatus.NOT_FOUND);
+    }
   }
   getPodcast(namePodcast: string): data[] {
-    return this.Data.filter((item) => item.podcastName === namePodcast);
+    try {
+      const fliteredData = this.Data.filter(
+        (item) => item.podcastName === namePodcast,
+      );
+      if (fliteredData) {
+        return fliteredData;
+      }
+    } catch (err) {
+      throw new HttpException(
+        'There is no category like that in your podcast list!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
